c9233f74835621d3fb7c4c0a39b11ebade37e100
Monique Aboumrad <moniqueabu@Moniques-MacBook-Pro-2.local>
2025-04-15 19:01:54 -0400

    added logo to landing page and added mobile friendly functionality for the header component

--- ai-final/src/components/header.js
+++ ai-final/src/components/header.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import styles from './header.module.css';
-
+import logo from './ailogo.svg';
+import { useState } from 'react';
 
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
     return (
         <header className={styles.header}>
             <div className={styles.logContainer}>
                 <img
-                src = '/ailogo.svg'
+                src = {logo}
                 alt = "AI Governance Hub Logo"
                 className={styles.logo}
                 />
                 <span className={styles.logoText}>AI Governance Hub</span>
             </div>
+
+            <div className={styles.hamburger} onClick={toggleMenu}>
+                <div className={styles.bar}></div>
+                <div className={styles.bar}></div>
+                <div className={styles.bar}></div>
+            </div>
+
             <nav className={styles.nav}>
                 <a href= "#"> Home</a>
                 <a href= "#"> Modules</a>
@@ -25,7 +38,23 @@ const Header = () => {
                 <button className={styles.login}>Log In</button>
                 <button className={styles.signup}>Sign Up</button>
             </div>
-        </header>
+
+            {isOpen && (
+            <div className={styles.mobileMenu}>
+                <a href= "#"> Home</a>
+                <a href= "#"> Modules</a>
+                <a href= "#"> Resources</a>
+                <a href= "#"> Community</a>
+                <a href= "#"> About Us</a>
+                <div className={styles.authButtons}>
+                <button className={styles.login}>Log In</button>
+                <button className={styles.signup}>Sign Up</button>
+                </div>
+            </div>
+
+        )
+        }
+    </header>
     );
 }
 
