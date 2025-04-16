571aecb26f58274f1cc0dce2580fe2f2a7729508
Monique Aboumrad <moniqueabu@Moniques-MacBook-Pro-2.local>
2025-04-16 11:50:42 -0400

    fixing small bugs on educational modules section

--- ai-final/src/components/educationalModules.js
+++ ai-final/src/components/educationalModules.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './educationalModules.modules.css';
+import styles from './educationalModules.module.css';
 import imgFairness from './imgFairness.png';
 
 
